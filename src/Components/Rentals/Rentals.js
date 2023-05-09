0429ec9f2fb19bce2075b79b410d97a6d901c8ba
minagrullon <minagrullon@pursuit.org>
2023-05-08 23:00:49 -0400

    rentals.js added

--- src/Components/Rentals/Rentals.js
+++ src/Components/Rentals/Rentals.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Rental from "./Rental";
+
+let API;
 
 export default function Rentals() {
-  return <div>Rentals</div>;
+  const [rentals, setRentals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/rentals`)
+      .then((res) => setRentals(res))
+      .catch((err) => console.warn(err));
+  }, []);
+  return (
+    <div className="rentals">
+      <h3>Rental Listings</h3>
+      {rentals.map((rental) => {
+        return <Rental key={rental.id} />;
+      })}
+    </div>
+  );
 }
