b2a31c58879a3918f266d6a3eaf5b1ec5f4002eb
minagrullon <minagrullon@pursuit.org>
2023-05-11 23:09:48 -0400

    some css for rentals and rental files

--- src/Components/Rentals/NewRental.js
+++ src/Components/Rentals/NewRental.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./RentalForm.css";
+import "./rentalForm.css";
 
 let API = process.env.REACT_APP_API_URL;
 
