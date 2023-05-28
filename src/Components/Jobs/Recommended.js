18e8b9aad9801d0eec58447787bc7115358d2c1b
Gillesedgy <edgygilles@pursuit.org>
2023-05-28 16:49:46 -0400

    Job details-> Map and Suggested styled. Need to fix or redo the form

--- src/Components/Jobs/Recommended.js
+++ src/Components/Jobs/Recommended.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./rec.css";
-export default function Recommended({ rec: { id, job_title, company } }) {
+
+export default function Recommended({ rec: { id, job_title, company }, icon }) {
   return (
     <div className="recommended">
       <Link to={`/jobs/${id}`}>
         {" "}
-        <h3>{job_title}</h3>
-        {company}
+        <div className="img-details">
+          <img className="img" src={icon} alt="job_icon" />
+          <h5>{job_title}</h5>@<p className="recommendedP">{company}</p>
+        </div>
       </Link>
     </div>
   );
