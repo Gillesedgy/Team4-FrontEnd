e65aa57368116acbc9a198e5193886bf6b4ba263
Gillesedgy <edgygilles@pursuit.org>
2023-06-04 10:26:46 -0400

    Logo select dropdown updated

--- src/Components/Jobs/LogoSelect.js
+++ src/Components/Jobs/LogoSelect.js
@@ -1,29 +1,39 @@
 import React from "react";
-import jobpic1 from "../../Assets/jobpic1.png";
-import jobpic2 from "../../Assets/jobpic2.png";
-import jobpic3 from "../../Assets/jobpic3.png";
-import jobpic4 from "../../Assets/jobpic4.png";
-import jobpic5 from "../../Assets/jobpic5.png";
-import jobpic6 from "../../Assets/jobpic6.png";
-import jobpic7 from "../../Assets/jobpic7.png";
-import jobpic8 from "../../Assets/jobpic8.png";
-import jobpic9 from "../../Assets/jobpic9.png";
+import "./LogoSelect.css";
+// Maintenance
+import HandTool from "../../Assets/HandTool.png";
+// Tutor / Teacher
 export const LogoSelect = ({ selectedLogo, handleLogoSelect }) => {
+  const shopper =
+    "https://www.nicepng.com/png/detail/395-3951102_bag-shop-shopper-shopping-icon-bag-the-black.png";
+  const finance =
+    "https://png.pngtree.com/png-vector/20190725/ourmid/pngtree-vector-money-saving-icon-png-image_1576708.jpg";
+  const educate =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjVg6yJQbpwP-io2MpAJrATPM1tFyfYsxWWA&usqp=CAU";
+  const talent =
+    "https://www.pngitem.com/pimgs/m/9-95013_transparent-idea-vector-png-black-and-white-design.png";
+  const childcare =
+    "https://i.pinimg.com/736x/1c/05/63/1c05634cf78ee7bb8d9c5288a2c6e58d.jpg";
+  const Construction =
+    "https://www.pngkey.com/png/detail/412-4120823_construction-worker-blue-collar-job-icon.png";
+  const clerical =
+    "https://static.vecteezy.com/system/resources/previews/000/356/947/original/workspace-vector-icon.jpg";
+
   const jobIcons = [
-    { value: jobpic1, label: "logo1" },
-    { value: jobpic2, label: "logo2" },
-    { value: jobpic3, label: "logo3" },
-    { value: jobpic4, label: "logo4" },
-    { value: jobpic5, label: "logo5" },
-    { value: jobpic6, label: "logo6" },
-    { value: jobpic7, label: "logo7" },
-    { value: jobpic8, label: "logo8" },
-    { value: jobpic9, label: "logo9" },
+    { value: shopper, label: "Deliveries/Errands/Shopping" },
+    { value: clerical, label: "Assistant/Office work" },
+    { value: finance, label: "Book-keeping/CPA/Finaces" },
+    { value: educate, label: "Instructor/Teaching/Tutor" },
+    { value: HandTool, label: "Maintenance/Mechanic/Repair" },
+    { value: Construction, label: "Construction" },
+    { value: talent, label: "Artist/Designer/Engineer" },
+    { value: childcare, label: "Babbysitter/Childcare" },
   ];
   return (
     <div className="job-logo">
       <label>
-        Logo: <img src={selectedLogo} alt="Selected Logo" />
+        Logo:{" "}
+        <img className="selected-logo" src={selectedLogo} alt="Selected Logo" />
       </label>
       <select value={selectedLogo} onChange={handleLogoSelect} required>
         <option value="">Select a logo</option>
--- src/Features/helper.js
+++ src/Features/helper.js
@@ -1,25 +1,25 @@
 // Icons
-import jobpic1 from "../Assets/jobpic1.png";
-import jobpic2 from "../Assets/jobpic2.png";
-import jobpic3 from "../Assets/jobpic3.png";
-import jobpic4 from "../Assets/jobpic4.png";
-import jobpic5 from "../Assets/jobpic5.png";
-import jobpic6 from "../Assets/jobpic6.png";
-import jobpic7 from "../Assets/jobpic7.png";
-import jobpic8 from "../Assets/jobpic8.png";
-import jobpic9 from "../Assets/jobpic9.png";
+// import jobpic1 from "../Assets/jobpic1.png";
+// import jobpic2 from "../Assets/jobpic2.png";
+// import jobpic3 from "../Assets/jobpic3.png";
+// import jobpic4 from "../Assets/jobpic4.png";
+// import jobpic5 from "../Assets/jobpic5.png";
+// import jobpic6 from "../Assets/jobpic6.png";
+// import jobpic7 from "../Assets/jobpic7.png";
+// import jobpic8 from "../Assets/jobpic8.png";
+// import jobpic9 from "../Assets/jobpic9.png";
 
 //Job Icons
 const jobIcons = [
-  jobpic1,
-  jobpic2,
-  jobpic3,
-  jobpic4,
-  jobpic5,
-  jobpic6,
-  jobpic7,
-  jobpic8,
-  jobpic9,
+  // jobpic1,
+  // jobpic2,
+  // jobpic3,
+  // jobpic4,
+  // jobpic5,
+  // jobpic6,
+  // jobpic7,
+  // jobpic8,
+  // jobpic9,
 ];
 export const handleIcons = () => {
   // randomize Icon
@@ -28,15 +28,15 @@ export const handleIcons = () => {
 };
 export const handleLogo = () => {
   const jobIcons2 = [
-    { value: jobpic1, label: "logo1" },
-    { value: jobpic2, label: "logo2" },
-    { value: jobpic3, label: "logo3" },
-    { value: jobpic4, label: "logo4" },
-    { value: jobpic5, label: "logo5" },
-    { value: jobpic6, label: "logo6" },
-    { value: jobpic7, label: "logo7" },
-    { value: jobpic8, label: "logo8" },
-    { value: jobpic9, label: "logo9" },
+    // { value: jobpic1, label: "logo1" },
+    // { value: jobpic2, label: "logo2" },
+    // { value: jobpic3, label: "logo3" },
+    // { value: jobpic4, label: "logo4" },
+    // { value: jobpic5, label: "logo5" },
+    // { value: jobpic6, label: "logo6" },
+    // { value: jobpic7, label: "logo7" },
+    // { value: jobpic8, label: "logo8" },
+    // { value: jobpic9, label: "logo9" },
   ];
   return jobIcons2;
 };
