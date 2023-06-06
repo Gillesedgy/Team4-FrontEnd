5ded390956d21f9d821d7cddf486a7a10124e2e7
dilrubamajumder <dilrubamajumder@pursuit.org>
2023-06-06 00:38:21 -0400

    login/logout functionality

--- src/Components/Nav/Button.js
+++ src/Components/Nav/Button.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Button.css";
 
+export default function Button({ user, handleLogout, closeMobileMenu }) {
+  const [ loggedIn, setLoggedIn ] = useState(false);
 
-export default function Button() {
+  useEffect(() => {
+    if(user?.token) {
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+  }, [user])
+
   return (
-    <Link to="login">
-      <button className="btn">Sign in</button>
-    </Link>
+    <>
+      {!loggedIn && (
+        <Link to="/login" onClick={closeMobileMenu}>
+          <button className="btn">Sign in</button>
+        </Link>
+      )}
+
+      {loggedIn && (
+        <Link onClick={handleLogout}>
+          <button className="btn">Logout</button>
+        </Link>
+      )}
+    </>
   );
 }
--- src/Provider.js
+++ src/Provider.js
@@ -1,4 +1,4 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 
 export const ContextData = createContext();
 export function useContextProvider() {
@@ -7,7 +7,6 @@ export function useContextProvider() {
 
 function Provider({ children }) {
   // A piece of state that will be passed to other files
-
   const [user, setUser] = useState({
     id: null,
     username: "",
@@ -16,6 +15,24 @@ function Provider({ children }) {
     address: "",
     native_language: "",
   });
+  
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      const user = JSON.parse(localStorage.getItem('user_info'))
+      setUser(user);
+    } else {
+      setUser({
+        id: null,
+        username: "",
+        email: "",
+        password: "",
+        address: "",
+        native_language: "",
+      });
+    }
+  }, []);
+
   return (
     <ContextData.Provider value={{ user, setUser }}>
       {children}
