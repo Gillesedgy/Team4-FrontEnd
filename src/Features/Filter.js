7e2cf0df428dd113c530c860706121270a2ba717
minagrullon <minagrullon@pursuit.org>
2023-06-17 12:39:01 -0400

    added language filter to listings

--- src/Features/Filter.js
+++ src/Features/Filter.js
@@ -1,6 +1,46 @@
 import React from "react";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
-export default function Filter() {
-  return <div>Filter</div>;
+import Rental from "../Components/Rentals/Rental";
+
+const API = process.env.REACT_APP_API_URL;
+
+export default function Filter({ selectedLanguage }) {
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    // if (selectedLanguage) {
+    axios
+      .get(`${API}/listings?language=${selectedLanguage}`)
+      .then((res) => {
+        let data = Object.values(res.data);
+        setFiltered(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    // } else if (!selectedLanguage) {
+    //   axios
+    //     .get(`${API}/listings`)
+    //     .then((res) => {
+    //       const recent = res.data.sort(
+    //         (a, b) => new Date(b.date_posted) - new Date(a.date_posted)
+    //       );
+    //       console.log("recent", recent);
+    //       setFiltered(recent);
+    //     })
+    //     .catch((err) => {
+    //       console.error(err);
+    //     });
+    // }
+  }, [selectedLanguage]);
+
+  return (
+    <>
+      {filtered.map((rental) => {
+        return <Rental key={rental.id} rental={rental} />;
+      })}
+    </>
+  );
 }
